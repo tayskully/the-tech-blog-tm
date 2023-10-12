3510e22f6d1e6885dfa9b28a1dd2716ab15eec7c
Taylor <taysully94@gmail.com>
2023-10-11 22:14:43 -0400

    adds changes to post and home routes, adds a logout JS, attempts to fix login logout bug, adds .env example

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,33 +1,40 @@
-const router = require('express').Router();
-const { Post } = require('../../models');
+const router = require("express").Router();
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // routes for api/posts/
 
-router.post('/', async (req, res) => {
-    const postData = await Post.findByPk(req.body);
-
-
-});
-
-
-router.delete('/:id', withAuth, async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
     try {
-      const projectData = await Project.destroy({
-        where: {
-          id: req.params.id,
-          user_id: req.session.user_id,
-        },
+      const newPost = await Post.create({
+        ...req.body,
+        user_id: req.session.user_id,
       });
   
-      if (!projectData) {
-        res.status(404).json({ message: 'No project found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(projectData);
+      res.status(200).json(newPost);
     } catch (err) {
-      res.status(500).json(err);
+      res.status(400).json(err);
     }
   });
 
-  module.exports = router;
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const projectData = await Project.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: "No project found with this id!" });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,7 @@ const { User, Post } = require("../models");
 router.get("/", async (req, res) => {
   try {
     const postData = await Post.findAll({
-      include: [User],
+      include: [User]
     });
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render("homepage", {
