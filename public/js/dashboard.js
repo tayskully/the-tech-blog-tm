dc084e908a1a68cea1326eb33bd69228187b2761
Taylor <taysully94@gmail.com>
2023-10-30 19:18:59 -0400

    changes dashboard js and handlebars to add post form

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,14 +1,28 @@
-
 async function addPost() {
   console.log("clicked!");
-const postForm = document.querySelector('#post-form');
+  const postForm = document.querySelector("#post-form");
+  const addPostBtn = document.querySelector("#add-post");
 
-postForm.innerHTML = ``
+  addPostBtn.classList.add("invisible");
+
+  postForm.innerHTML = `  <div class="form-floating">
+<textarea class="form-control" id="post-title" placeholder="Title" id="floatingTextarea"></textarea>
+<label for="floatingTextarea">Title of Post</label>
+</div>
+<div class="form-floating">
+<textarea class="form-control" id="post-text" placeholder="Write your post here" id="floatingTextarea2" style="height: 100px"></textarea>
+<label for="floatingTextarea2">What are your thoughts?</label>
+</div>  <button
+class="btn btn-light float-end fs-1 focus-ring py-1 px-2 mx-3"
+id="submit-post"
+>Publish</button>`;
   const post_text = document.querySelector("#post-text").value.trim();
-  const post_title = document.querySelector("#post_title").value;
+  const post_title = document.querySelector("#post-title").value;
 
-  if (comment_text) {
-    const response = await fetch("/api/comments/", {
+  //i don't remember where i left off here / post request for new blog post that uses title and post
+  //and user and date and ....
+  if (post_text) {
+    const response = await fetch("/api/posts/", {
       method: "POST",
       body: JSON.stringify({ comment_text, post_id }),
       headers: { "Content-Type": "application/json" },
@@ -22,17 +36,14 @@ postForm.innerHTML = ``
   }
 }
 
-async function deletePost () {
-  console.log("delete clicked!")
-
+async function deletePost() {
+  console.log("delete clicked!");
 }
 
 async function editPost() {
-  console.log("editing")
+  console.log("editing");
 }
-
 
 document.querySelector("#edit").addEventListener("click", editPost);
 document.querySelector("#delete").addEventListener("click", deletePost);
 document.querySelector("#add-post").addEventListener("click", addPost);
-
